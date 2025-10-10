1a5fa172457fca6957a38d685b9f021377a08af6
SergioFraga <sergiozfraga@gmail.com>
2025-10-10 15:25:59 -0700

    Se elimina el archivo Dashboard.jsx para no generar conflictos con el otro dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import LoginPage from "./feature/auth/LoginPage";
 import { PatientProvider } from "./context/PatientContext";
-import Dashboard from "./feature/Dashboard";
 import { ProtectedRoutePatient } from "./routes/ProtectedRoutePatient";
 
 function App() {
@@ -13,7 +12,7 @@ function App() {
           <Routes>
             <Route path="/" element={<LoginPage />} />
             <Route element={<ProtectedRoutePatient />}>
-              <Route path="/dashboard" element={<Dashboard />} />
+              {/*Se agregan las rutas que solo puede acceder el paciente*/}
             </Route>
           </Routes>
         </BrowserRouter>
