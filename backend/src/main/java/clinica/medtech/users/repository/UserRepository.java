38d7c7279125926012b70c031c9474395bca493a
isabel prudencio <isabel.bits@gmail.com>
2025-10-12 23:13:22 -0500

    Agregando filtros de paciente

--- backend/src/main/java/clinica/medtech/users/repository/UserRepository.java
+++ backend/src/main/java/clinica/medtech/users/repository/UserRepository.java
@@ -2,6 +2,7 @@ package clinica.medtech.users.repository;
 
 
 import clinica.medtech.users.Enum.EnumRole;
+import clinica.medtech.users.entities.PatientModel;
 import clinica.medtech.users.entities.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -15,4 +16,16 @@ public interface UserRepository extends JpaRepository<UserModel, Long> {
     List<UserModel> findAllBySuspensionEndBefore(LocalDateTime now);
 
     List<UserModel> findUsersByRolesEnumRole(EnumRole enumRole);
+
+    // Filtrar pacientes por nombre
+    List<PatientModel> findByNameAndRolesEnumRole(String name, EnumRole enumRole);
+
+    // Filtrar pacientes por ID
+    Optional<PatientModel> findByIdAndRolesEnumRole(Long id, EnumRole enumRole);
+
+    // Filtrar pacientes por email
+    Optional<PatientModel> findByEmailAndRolesEnumRole(String email, EnumRole enumRole);
+
+    // Filtrar pacientes por apellido
+    List<PatientModel> findByLastNameAndRolesEnumRole(String lastName, EnumRole enumRole);
 }
